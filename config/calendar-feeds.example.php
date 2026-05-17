7230c2c68d6d2fa294880e060a8bc70a6c61de52
Hernán Gobulin <gobulinhernan@gmail.com>
2026-05-17 18:54:30 -0300

    Add private calendar feed config example

---
+++ config/calendar-feeds.example.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Example Calendar feeds configuration.
+ *
+ * Copy this file as calendar-feeds.php and replace the sample URLs
+ * with your private Google Calendar iCal URLs.
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+return array(
+    array(
+        'name' => 'CALL',
+        'url' => '',
+    ),
+    array(
+        'name' => 'Grabaciones',
+        'url' => '',
+    ),
+    array(
+        'name' => 'Masterclass',
+        'url' => '',
+    ),
+);
